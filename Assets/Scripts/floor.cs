7e25a2b19c8a181bc8ed9ef1038f27f4990024a9
Adam Hall <adamhalldesigns@gmail.com>
2019-03-23 21:06:46 +0000

    setup project

---
+++ Assets/Scripts/floor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor : MonoBehaviour
+{
+    private Transform body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(0, 1, 0);
+        body.Rotate(rotation);
+
+    }
+}
